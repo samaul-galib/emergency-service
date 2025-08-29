560f8655c38e30dca9bda072b26d396a090b0217
Md Samaul Galib <samaulgalib1971@gmail.com>
2025-08-29 09:37:19 +0600

    functions added

--- script.js
+++ script.js
@@ -1,8 +1,46 @@
 let totalHearts = 0;
-const navHeartCount = document.getElementById('heart-click-count');
+const heartClickCount = document.getElementById('heart-click-count');
 const cardHearts = document.querySelectorAll('.card-heart');
 
-document.addEventListener('click', function(){
+cardHearts.forEach(function(img) {
+img.addEventListener('click', function(){
 totalHearts++;
-  navHeartCount.textContent = totalHearts;
+  heartClickCount.textContent = totalHearts;
   });
+});
+
+let totalCoins = 100;
+const navCoinCount = document.getElementById('coin-count');
+const cardCoins = document.querySelectorAll('.card-coin');
+
+navCoinCount.textContent = totalCoins;
+
+cardCoins.forEach(function(img) {
+  img.onclick = function () {
+    if (totalCoins >= 20) {
+      totalCoins -= 20;
+      navCoinCount.textContent = totalCoins;
+      alert("You spent 20 coins!");
+    } else {
+      alert("You don't have enough coins to call");
+    }
+  };
+});
+
+let totalCopy = 0;
+const copyCount = document.getElementById('copy-count');
+const buttons = document.querySelectorAll('.card-copy');
+
+buttons.forEach(function(button) {
+  button.addEventListener('click', function() {
+    const number = button.closest('.card-body').querySelector('h2').innerText;
+    
+    navigator.clipboard.writeText(number);
+
+    totalCopy++;
+    copyCount.textContent = totalCopy;
+
+    alert("Copied: " + number);
+  });
+});
+
